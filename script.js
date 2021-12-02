f2dd50651ab6279ab081ec5261bc5f9f89ce0869
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2021-12-02 19:27:37 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const enviar = document.getElementById('buttonEnter');
 const email = document.getElementById('email');
 const senha = document.getElementById('password');
+const counter = document.getElementById('counter');
+const textArea = document.getElementById('textarea');
+
 function verificaEmail() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     window.alert('Olá, Tryber!');
@@ -10,3 +13,13 @@ function verificaEmail() {
 }
 enviar.addEventListener('click', verificaEmail);
 console.log('aaaaa');
+
+function counterStrings() {
+  const count = textArea.value.length;
+  console.log(count);
+  const restante = 500 - count;
+  counter.innerText = restante;
+}
+
+textArea.addEventListener('keyup', counterStrings);
+console.log(counter);
