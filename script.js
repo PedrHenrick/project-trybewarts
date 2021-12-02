9ceae5a0a19ac78704e2571b3b4a23bd83f7d96a
Pedro Henrick <phmc@ic.ufal.br>
2021-12-02 16:31:04 -0300

    Requisitos 3 e 4

---
+++ script.js
@@ -0,0 +1,11 @@
+const enviar = document.getElementById('buttonEnter');
+const email = document.getElementById('email');
+const senha = document.getElementById('password');
+function verificaEmail() {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+}
+enviar.addEventListener('click', verificaEmail);
